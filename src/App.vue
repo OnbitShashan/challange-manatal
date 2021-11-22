2e740efa108b7e3b5be11e6744bc5284fea65b4f
OnbitShashan <kanishka.shashan@gmail.com>
2021-11-23 00:11:58 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="indigo" dark>
+      <div class="d-flex align-center">
+        <v-btn class="mr-3" text href="/">
+          <v-icon color="white">mdi-home</v-icon>
+        </v-btn>
+        <h2>News</h2>
+      </div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+      <!-- <HelloWorld/> -->
+    </v-main>
+  </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/NewsHeadlines.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <h1 class="mb-3 indigo--text text--lighten-1 font-weight-light">Headlines</h1>
+    <v-row class="text-center">
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <v-card>
+          <v-img
+            :src="'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F619bc3ad4e8a0f5ca49de1ea%2F0x0.jpg'"
+            class="white--text"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          >
+            <v-card-title
+              class="text-break"
+              v-text="'Rivian Vs. Tesla When They Crossed $100 Billion Valuations'"
+            ></v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn text href="/details">
+              Read Now
+              <v-icon color="green darken-2">mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'NewsHeadlines',
+  components: {
+    // HelloWorld
+  }
+}
+</script>

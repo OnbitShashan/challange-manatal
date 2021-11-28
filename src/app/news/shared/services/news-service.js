53a4b50e57f7ebc9629b8a249ddd9f802f3a8a63
OnbitShashan <kanishka.shashan@gmail.com>
2021-11-28 20:02:21 +0530

    file rename to news-service

--- src/app/news/shared/services/news-service.js
+++ src/app/news/shared/services/news-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { API_CONSTANTS, API_KEY } from '../config';
-
+  
 const fetchNewsTopHeadlines = async (params) => {
   return await axios.get(API_CONSTANTS.fetechNewsTopHeadlines, {
     params: {

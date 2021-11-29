e84303565e64ed7c5d9e8d4922de474a4b1eb666
OnbitShashan <kanishka.shashan@gmail.com>
2021-11-29 16:47:03 +0530

    Enhanced ui and added details to the local storage

--- src/app/news/news-details/news-details.vue
+++ src/app/news/news-details/news-details.vue
@@ -3,9 +3,19 @@
     <v-row v-if="newsDetails">
       <v-col class="text-center" cols="12">
         <h1 class="my-5 indigo--text text--darken-2 font-weight-regular">{{ newsDetails.title }}</h1>
-        <v-parallax v-if="newsDetails.urlToImage" :src="newsDetails.urlToImage"></v-parallax>
+        <v-img
+          :aspect-ratio="16 / 9"
+          width="100%"
+          v-if="newsDetails.urlToImage"
+          :src="newsDetails.urlToImage"
+        ></v-img>
       </v-col>
-      <v-col cols="12">{{ newsDetails.content }}</v-col>
+      <v-col cols="12">
+        <div class="text-overline">Author: {{ newsDetails.author }}</div>
+        <div class="text-caption font-italic">Published at: {{ formatTimeToUTCString(newsDetails.publishedAt) }}</div>
+        <blockquote class="blockquote font-weight-bold">{{ newsDetails.description }}</blockquote>
+        <p>{{ newsDetails.content }}</p>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -23,6 +33,20 @@ export default {
     };
   },
   mounted: function () {
+    // MANAGE NEWS_DETAILS
+    if (!this.details && appLocalStorage.getItem('lastViewdHeadline')) {
+      try {
+        this.newsDetails = appLocalStorage.getItem('lastViewdHeadline');
+      } catch (e) {
+        appLocalStorage.removeItem('lastViewdHeadline');
+      }
+    }
+
+    if (this.details) {
+      appLocalStorage.setItem('lastViewdHeadline', this.details);
+    }
+
+    // GET AND SET VISITED TITLES
     if (appLocalStorage.getItem('visitedLinks')) {
       try {
         this.visitedLinks = appLocalStorage.getItem('visitedLinks');
@@ -35,7 +59,11 @@ export default {
       this.visitedLinks.push({ date: new Date().toUTCString(), title: this.newsDetails.title });
       appLocalStorage.setItem('visitedLinks', this.visitedLinks);
     }
-
+  },
+  methods: {
+    formatTimeToUTCString: function (time) {
+      return new Date(time).toUTCString();
+    }
   }
 }
 </script>
